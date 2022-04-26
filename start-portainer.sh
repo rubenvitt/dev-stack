19175af2bd4b5a6d5db12b8e5c62b4553ad11b85
Ruben Vitt <dev@rubeen.one>
2022-04-26 10:03:26 +0200

    add portainer

--- start-portainer.sh
+++ start-portainer.sh
@@ -1,5 +1,7 @@
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always \
+docker run -d --expose 9000 --name=portainer --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
     --network=hosting-network \
+    -e VIRTUAL_HOST=portainer.rubeen.dev \
+    -e VIRTUAL_PORT=9000 \
     portainer/portainer-ee:latest
